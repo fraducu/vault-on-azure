a63b7576948fea556c01d98d6b99bef42f95344e
fraducu <radu.farcas@outlook.com>
2019-02-11 08:51:23 +0000

    commented out the ssh key vars

--- variables.tf
+++ variables.tf
@@ -43,6 +43,6 @@ variable "vm_admin" {
     //Disallowed values: "administrator", "admin", "user", "user1", "test", "user2", "test1", "user3", "admin1", "1", "123", "a", "actuser", "adm", "admin2", "aspnet", "backup", "console", "david", "guest", "john", "owner", "root", "server", "sql", "support", "support_388945a0", "sys", "test2", "test3", "user4", "user5".
 }
 
-variable "ssh_key_public" {}
+#variable "ssh_key_public" {}
 
-variable "ssh_key_private" {}
+#variable "ssh_key_private" {}
